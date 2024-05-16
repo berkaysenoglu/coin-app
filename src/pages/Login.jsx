a814aba351892dcc2f63f4713910966bb5510c04
berkaysenoglu <berkaykazanan@outlook.com>
2024-05-17 02:47:32 +0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,88 +29,78 @@ export const Login = () => {
   };
 
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
+    <div className="form-wrapper">
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
         }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={handleLogin}
+        autoComplete="off"
       >
-        <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
           wrapperCol={{
+            offset: 8,
             span: 16,
           }}
-          style={{
-            maxWidth: 600,
+        ></Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
           }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={handleLogin}
-          autoComplete="off"
         >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Please input your email!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your password!",
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          ></Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit" loading={loading}>
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-        {/* <form action="">
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      {/* <form action="">
         <Input placeholder="email" />
         <Input.Password placeholder="input password" />
         <Button type="primary" htmlType="submit">
           Submit
         </Button>
       </form> */}
-      </div>
-    </>
+    </div>
   );
 };
 
