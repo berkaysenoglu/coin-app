bb89d284bff641229204a1db918cff9608c5db03
berkaysenoglu <berkaykazanan@outlook.com>
2024-05-24 22:37:11 +0300

    coin api fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const { user } = useAuth();
+  const [coins, setCoins] = useState([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/coins");
+      if (response.status === 200) {
+        setCoins(response.data);
+      } else {
+        message.error(`Hata: ${response.data.message}`);
+      }
+    } catch (error) {
+      message.error(`Hata: ${error.message}`);
+    }
+  };
 
-  return <div>Home</div>;
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(coins);
+  return (
+    <div>
+      {coins.map((coin) => (
+        <a>{coin.name}</a>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
